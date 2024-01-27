32106432695eaa52e64383ace8a8bdb5ed8530e7
Pumehana Cartagena <pum3hanacarta@byui.edu>
2024-01-27 17:37:33 -0500

    wk3 updates, POST, PUT, DELETE

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -22,4 +22,65 @@ const getSingle = async (req, res, next) => {
   });
 };
 
-module.exports = { getAll, getSingle };
+//POST
+const createContact = async (req, res, next) => {
+//send a json body
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+  const response = await mongodb.getDb().db().collection('contacts').insertOne(contact);
+  if (response.acknowledged) {
+    res.status(204).json(response);
+  }else{
+    res.status(500).json(response.error || 'Error while creating contact.');
+  }
+};
+
+//PUT
+const updateContact = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+  // be aware of updateOne if you only want to update specific fields
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+  const response = await mongodb
+    .getDb()
+    .db()
+    .collection('contacts')
+    .replaceOne({ _id: userId }, contact);
+  console.log(response);
+  if (response.modifiedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while updating the contact.');
+  }
+};
+
+//DELETE
+const deleteContact = async (req, res, next) => {
+  const userID = new ObjectId(req.params.id);
+  //.remove did not work, instead used .deleteOne, but there is also .deleteMany to delete more than 1
+  const response = await mongodb.getDb().db().collection('contacts').deleteOne({ _id: userID }, true);
+  console.log(response);
+  if (response.deletedCount > 0) {
+    res.status(204).send();
+  }else{
+    res.status(500).json(response.error || 'Error during deleting contact.');
+  }
+};
+
+module.exports = { 
+  getAll, 
+  getSingle,
+  createContact,
+  updateContact,
+  deleteContact
+};
--- routes/contacts.js
+++ routes/contacts.js
@@ -7,4 +7,13 @@ router.get('/', contactsController.getAll);
 
 router.get('/:id', contactsController.getSingle);
 
+//week 3 additions: POST, PUT, DELETE
+
+router.post('/', contactsController.createContact);
+
+router.put('/:id', contactsController.updateContact);
+
+router.delete('/:id', contactsController.deleteContact);
+//end of week 3 additions
+
 module.exports = router;
